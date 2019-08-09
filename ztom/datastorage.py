ebdabac1fdeb61e38b23208c06da45fa684a5932
ztom <tkgzlad@protonmail.com>
2019-08-09 19:11:09 +0300

    datastorage: added option to overwrite file or append. Also, new pull_path field

--- ztom/datastorage.py
+++ ztom/datastorage.py
@@ -29,21 +29,22 @@ class DataStorage:
         self.folder = folder
         self.entities = collections.defaultdict(str)
 
-    def register(self, type_name, headers):
+    def register(self, type_name, headers, overwrite=True):
         # self.validate_not_exists(type_name)
         full_path = os.path.join(self.folder, type_name + ".csv")
         exists = False
-        if os.path.exists(full_path):
+        if os.path.exists(full_path) and not overwrite:
             file = open(full_path, "a", newline='')
             exists = True
         else:
             file = open(full_path, "w", newline='')
 
         writer = csv.writer(file)
+
         if not exists:
             writer.writerow(headers)
 
-        self.entities[type_name] = {'file': file, 'writer': writer, 'headers': headers}
+        self.entities[type_name] = {'file': file, 'writer': writer, 'headers': headers, "full_path":full_path}
 
     def save(self, type_name, row):
         self.validate_exists(type_name)
