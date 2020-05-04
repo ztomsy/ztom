5b0ac427b87abad23a25ef086a6575dc3aa97614
ztom <tkgzlad@protonmail.com>
2020-05-04 10:30:27 +0300

    check for minimum filled amount

--- ztom/fok_order.py
+++ ztom/fok_order.py
@@ -41,16 +41,18 @@ class FokOrder(ActionOrder):
         if self.time_to_cancel > 0.0:
 
             if self.time_from_create >= self.time_to_cancel \
-                    and active_trade_order.amount - active_trade_order.filled  > self.cancel_threshold:
-                    # and (active_trade_order.filled > self.cancel_threshold or active_trade_order.filled == 0):
+                    and (active_trade_order.amount - active_trade_order.filled >= self.cancel_threshold
+                         or active_trade_order.amount <= self.cancel_threshold) \
+                    and (active_trade_order.filled > self.cancel_threshold or active_trade_order.filled == 0):
                 self.tags.append("#timeout")
                 return "cancel"
             else:
                 return "hold"
 
         if active_trade_order.update_requests_count >= self.max_order_updates \
-                and active_trade_order.amount - active_trade_order.filled > self.cancel_threshold:
-                # and (active_trade_order.filled > self.cancel_threshold or active_trade_order.filled == 0):
+                and (active_trade_order.amount - active_trade_order.filled >= self.cancel_threshold
+                     or active_trade_order.amount <= self.cancel_threshold) \
+                and (active_trade_order.filled > self.cancel_threshold or active_trade_order.filled == 0):
             return "cancel"
         return "hold"
 
@@ -170,8 +172,8 @@ class FokThresholdTakerPriceOrder(FokOrder):
                                                                        current_taker_price)
 
                     if price_diff is not None and price_diff <= self.taker_price_threshold \
-                            and active_trade_order.amount - active_trade_order.filled > self.cancel_threshold:
-                            #and (active_trade_order.filled > self.cancel_threshold or active_trade_order.filled ==0):
+                            and active_trade_order.amount - active_trade_order.filled > self.cancel_threshold \
+                            and (active_trade_order.filled > self.cancel_threshold or active_trade_order.filled ==0):
 
                         order_command = "cancel"
 
