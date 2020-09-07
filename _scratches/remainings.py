c9cf14d56d21ba656a4890e519a19d6575e81a65
Ivan Averin <i.averin@gmail.com>
2020-09-08 01:18:11 +0300

    added remainings scratch, moved _live_tests to scratches

---
+++ _scratches/remainings.py
@@ -0,0 +1,41 @@
+import ztom
+from ztom import Remainings
+from sqlalchemy.exc import IntegrityError
+
+reporter = ztom.SqlaReporter("test", "testEx")
+
+connection_string = "postgres://postgres:12345@localhost:5432/ztom"
+
+reporter.init_db(connection_string, echo=True)
+
+print("Tables in db: {}".format(list(reporter.metadata.tables.keys())))
+
+
+tables = reporter.create_tables()
+print("Created tables {}".format(tables))
+
+remainings = Remainings(
+
+    exchange="binance",
+    account="test",
+    currency="BTC",
+    symbol="BTC/USDT",
+    target_currency="USDT"
+
+)
+
+
+reporter.session.add(remainings)
+
+try:
+    reporter.session.commit()
+except IntegrityError as e:
+    print("Integrity error")
+
+"""
+TODO: The flow: 
+- try to update 
+- try to insert if prev update failed
+- try to update in prev insert failed 
+"""
+
