0893ec6512193ae1f3ffe0c327324082e31a6352
Ivan Averin <i.averin@gmail.com>
2023-02-19 20:36:50 +0300

    update internal import

--- ztom/__init__.py
+++ ztom/__init__.py
@@ -1,36 +1,34 @@
-from ztom import *
-from ztom.cli import *
-from ztom.timer import Timer
-from ztom.utils import *
-from ztom.exchanges import *
-from ztom.exchange_wrapper import ccxtExchangeWrapper
-from ztom.exchange_wrapper import ExchangeWrapperOfflineFetchError
-from ztom.exchange_wrapper import ExchangeWrapperError
-from ztom.stats_influx import StatsInflux
-from ztom.datastorage import DataStorage
-from ztom.reporter import Reporter, MongoReporter
-from ztom.models.deal import DealReport
-from ztom.models.trade_order import TradeOrderReport
-from ztom.models.tickers import Tickers
-from ztom.reporter_sqla import SqlaReporter
-from ztom.orderbook import OrderBook
-from ztom.orderbook import Order
-from ztom.orderbook import Depth
-from ztom.trade_orders import *
-from ztom.trade_order_manager import *
-from ztom import core
-from ztom.bot import Bot
-from ztom.errors import *
-from ztom.action_order import ActionOrder
-from ztom.recovery_orders import RecoveryOrder
-from ztom.fok_order import FokOrder, FokThresholdTakerPriceOrder
-from ztom.order_manager import *
-from ztom.throttle import Throttle
-from ztom import *
-from ztom.models.remainings import Remainings
+from .cli import *
+from .timer import Timer
+from .utils import *
+from .exchanges import *
+from .exchange_wrapper import ccxtExchangeWrapper
+from .exchange_wrapper import ExchangeWrapperOfflineFetchError
+from .exchange_wrapper import ExchangeWrapperError
+from .stats_influx import StatsInflux
+from .datastorage import DataStorage
+from .reporter import Reporter, MongoReporter
+from .models.deal import DealReport
+from .models.trade_order import TradeOrderReport
+from .models.tickers import Tickers
+from .reporter_sqla import SqlaReporter
+from .orderbook import OrderBook
+from .orderbook import Order
+from .orderbook import Depth
+from .trade_orders import *
+from .trade_order_manager import *
+from . import core
+from .bot import Bot
+from .errors import *
+from .action_order import ActionOrder
+from .recovery_orders import RecoveryOrder
+from .fok_order import FokOrder, FokThresholdTakerPriceOrder
+from .order_manager import ActionOrderManager
+from .throttle import Throttle
+from .models.remainings import Remainings
 
 # Legacy support
-from ztom.owa_manager import OwaManager
-from ztom.owa_orders import OrderWithAim
+from .owa_manager import OwaManager
+from .owa_orders import OrderWithAim
 
 
